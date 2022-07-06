189874727219b08b423d32dc9bbd75a30ca55e5c
maleekaFatima02 <maleekafatima02@gmail.com>
2022-07-06 14:23:36 +0500

    heroku issue

--- src/app.js
+++ src/app.js
@@ -1,23 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const ngrok = require('ngrok');
-
 
 app.get('/', (req, res) => {
     res.send('Hello World I am running locally');
 });
 
-const server = app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('Running at 8080');
-});
-
-ngrok.connect({
-    proto : 'http',
-    addr : process.env.PORT,
-}, (err, url) => {
-    if (err) {
-        console.error('Error while connecting Ngrok',err);
-        return new Error('Ngrok Failed');
-    }
 });
